cf8af45a505c0de10221ee7a701abff387fa81b8
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2020-09-07 15:30:03 +0300

    Create app.js
    Passing a Variable to ReactDOM.render()

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+var myList=(
+<ul>
+<li>Pizza</li>
+<li>Chocolate</li>
+<li>Ugali</li>
+<li>Chips</li>
+
+</ul>
+);
+ReactDOM.render(
+  myList,
+  document.getElementById('app')
+);
